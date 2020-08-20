4f12fb8f1d86bbd17f6edec2a62aba3eb2af625b
Michiel Boland <michiel@boland.org>
2020-08-20 21:13:35 +0200

    pedantic square

--- square.s
+++ square.s
@@ -6,15 +6,23 @@
 ; So to output something that looks like a square you need to draw
 ; a 171x160 rectangle.
 
+; you could argue that c64 pixel aspect is really 8640000 : 9221927
+; (768/52/2 : 4*4.43361875/18*8)
+; which is approximately 193 / 206
+; In that case a square would be 206x193 pixels
+
 cls = $e544
 print = $ab1e
 
 rvs_on = 18
+home = 19
 rvs_off = rvs_on + $80
 ; 3 pixels wide
 bar = 181
 bar2 = 182
 cr = 13
+stripe1 = 163
+bar3 = 170
 
   lda #12
   sta 53280
@@ -24,32 +32,31 @@ cr = 13
   jsr cls
   lda #<s
   ldy #>s
-  ldx #17
+  ldx #24
 next
   jsr printline
   dex
   bne next
-  rts
+  lda #<t
+  ldy #>t
+  jmp print
 printline
   stx tmp
-  lda #<t
-  clc
-  cld
-  adc tmp
-  ldy #>t
-  bcc .l0
-  iny
-.l0
-  jsr print
   lda #<s
   ldy #>s
   jsr print
   ldx tmp
   rts
-t
-  .byte "                 ", 0
 s
-  .byte rvs_on, "  ", bar2, rvs_off, "                  "
-  .byte rvs_on, bar, "  ", rvs_off,  cr, 0
+  .byte rvs_on
+  .rept 25
+  .byte " "
+  .endr
+  .byte bar3, cr, 0
+t
+  .rept 25
+  .byte stripe1
+  .endr
+  .byte home, 0
 tmp
   .byte 0
