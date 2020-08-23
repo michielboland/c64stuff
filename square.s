1c394791228d3129944a2af6fc469cdc82fc7d86
Michiel Boland <michiel@boland.org>
2020-08-23 13:21:19 +0200

    switch to sprites in square.s

--- square.s
+++ square.s
@@ -24,39 +24,32 @@ cr = 13
 stripe1 = 163
 bar3 = 170
 
-  lda #12
-  sta 53280
-  sta 53281
-  lda #11
-  sta 646
   jsr cls
-  lda #<s
-  ldy #>s
-  ldx #24
-next
-  jsr printline
+  ldx #62
+  lda #255
+.l0
+  sta 832, x
   dex
-  bne next
-  lda #<t
-  ldy #>t
-  jmp print
-printline
-  stx tmp
-  lda #<s
-  ldy #>s
-  jsr print
-  ldx tmp
-  rts
-s
-  .byte rvs_on
-  .rept 25
-  .byte " "
-  .endr
-  .byte bar3, cr, 0
-t
-  .rept 25
-  .byte stripe1
-  .endr
-  .byte home, 0
-tmp
-  .byte 0
+  bpl .l0
+  ldx #7
+  lda #13
+.l1
+  sta 2040, x
+  dex
+  bpl .l1
+  ldx #46
+.l2
+  lda vic, x
+  sta 53248, x
+  dex
+  bpl .l2
+.l3
+  bmi .l3
+
+vic
+  .byte  24,  50, 195,  50,  24, 210, 195, 210
+  .byte 114,  50,  64,  50, 114, 243,  64, 243
+  .byte 160, 155,  56,   0,   0, 255,   8,   0
+  .byte  20,  15,   0,   0,   0,   0,   0,   0
+  .byte  12,  12,  12,  12,  12,  12,  12,  11
+  .byte  11,  11,  11,  15,  15,  15,  15
