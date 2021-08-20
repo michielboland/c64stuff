ff84551fe2a6c07860f9e1f5f21e03ce85b7c681
Michiel Boland <michiel@boland.org>
2021-08-20 23:09:51 +0200

    Add vic20rasterscan_ntsc.prg

---
+++ asm/vic20rasterscan_ntsc.s
@@ -0,0 +1,122 @@
+  .include "vic20bootstrap.s"
+
+rc = $9004
+
+screen = $1e00
+color_ram = $9600
+p = 251
+q = 253
+
+  jmp start
+
+register
+  .byte 4
+line
+  .byte 126
+color
+  .byte 6
+cycles
+  .word 505
+
+start
+  lda #8
+  sta $900f
+
+  sei
+
+init
+  lda #0
+  sta offset
+  sta offset+1
+  sta p
+  sta q
+  lda #>screen
+  sta p+1
+  lda #>color_ram
+  sta q+1
+
+loop
+  lda line
+notras
+  cmp rc
+  bne notras
+ras
+  cmp rc
+  beq ras
+  jsr delay
+  bit $9003
+  bmi skip4a
+  bit 0
+  nop
+skip4a
+  jsr delay
+  bit $9003
+  bmi *+2
+  bmi *+2
+  jsr delay
+  bit $9003
+  bpl *+2
+
+  lda offset+1
+  sta tmp
+  ldy offset
+  tya
+  lsr tmp
+  ror
+  lsr tmp
+  ror
+  lsr tmp
+  ror
+  tax
+  inx
+delay8
+  bit 0
+  dex
+  bpl delay8
+  tya
+  lsr
+  bcs *+2
+  lsr
+  bcs *+2
+  bcs *+2
+  lsr
+  bcc skip4b
+  bit 0
+  nop
+skip4b
+  inc $900f
+  dec $900f
+  ldx register
+  lda $9000,x
+  sta (p),y
+  lda color
+  sta (q),y
+  eor #4
+  sta color
+  iny
+  sty offset
+  bne compare
+  inc p+1
+  inc q+1
+  inc offset+1
+compare
+  cpy cycles
+  bcc next
+  lda offset+1
+  sbc cycles+1
+  bcs reset
+next
+  jmp loop
+reset
+  jmp init
+
+delay
+  ldy #8
+.l
+  dey
+  bne .l
+  bit 0
+  rts
+
+offset = *
+tmp = * + 2
