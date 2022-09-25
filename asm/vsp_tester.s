0367a792a99989af26ff843500eb9bc6a2a1ae47
Michiel Boland <michiel@boland.org>
2022-09-25 23:58:24 +0200

    Slightly adjust vsp_tester

--- asm/vsp_tester.s
+++ asm/vsp_tester.s
@@ -2,66 +2,38 @@
 
 * = $e000
 
-YOFFSET = 0
-YBITS = %01011000
-
-  lda #128
-  sta 54290
-  sta 54287
+YOFFSET = 3
+YBITS = %00011000
 
   ldx #0
-  lda #1
 clr
+  txa
+  sta $0400,x
+  sta $0500,x
+  sta $0600,x
+  sta $0700,x
+  lda #14
   sta $d800,x
   sta $d900,x
   sta $da00,x
   sta $db00,x
   inx
   bne clr
-  inx
-clr2
-  lda 54299
-  .byte $9d,1,0 ; sta $0001,x
-  sta $0100,x
-  sta $01ff,x
-  sta $02fe,x
-  sta $03fd,x
-  sta $04fc,x
-  sta $05fb,x
-  sta $06fa,x
-  sta $07f9,x
-  sta $08f8,x
-  sta $09f7,x
-  sta $0af6,x
-  sta $0bf5,x
-  sta $0cf4,x
-  sta $0df3,x
-  sta $0ef2,x
-  sta $0ff1,x
-  inx
-  bne clr2
   lda #8
   sta 53270
-  lda #25
+  lda #((YOFFSET + 1) & 7) | YBITS
   sta 53265
   lda #47
   sta 53266
   lda #28
   sta 53272
-  lda #15
+  lda #14
   sta 53280
-  lda #0
+  lda #6
   sta 53281
-  lda #7
-  sta 53282
-  lda #8
-  sta 53283
-  lda #9
-  sta 53284
-  
 
   .macro DELAY
-  .rept 1
+  .rept 4
   nop
   .endr
   .endm
@@ -72,9 +44,8 @@ clr2
   bne *-3
   DELAY
   lda #((\2 + YOFFSET) & 7) | YBITS
-  ldy #((\2 + \3 + YOFFSET) & 7) | YBITS | 32
+  ldy #((\2 + \3 + YOFFSET) & 7) | YBITS
   sty 53265
-  sta 53281
   sta 53265
   .endm
 
@@ -121,20 +92,20 @@ loop
   VSP 219, 6
   VSP 228, 7
   VSP 237, 1
-  BADLINE 246, 6, 2
+  .if YOFFSET < 3
+  VSP 246, 2
+  .endif
+  lda #((YOFFSET + 1) & 7) | YBITS
+  sta 53265
 
   jmp loop
 
 * = $f000
   .include "binary_charset.s"
 
-* = $ff00
-
-  jmp $e000
-
 * = $fffa
 
-  .word $ff00
-  .word $ff00
-  .word $ff00
+  .word $e000
+  .word $e000
+  .word $e000
 
