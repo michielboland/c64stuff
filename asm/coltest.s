44dfa2a7547db22d76b6901c0c9ac21d3d5892f2
Michiel Boland <michiel@boland.org>
2023-03-02 19:47:27 +0100

    coltest: Simplify, and explain what it tries to do

--- asm/coltest.s
+++ asm/coltest.s
@@ -1,5 +1,10 @@
   .include "bootstrap.s"
 
+; on 6569R1 this will display two 8-character wide bars in the top row
+; with the contents of $3fff between them
+;
+; ..********...**...********
+
 irqvec = $0314
 
   sei
@@ -17,69 +22,60 @@ irqvec = $0314
   sta irqvec
   lda #>isr
   sta irqvec+1
-  lda #23
-  sta x
-  lda #20
-  sta y
   lda #0
-  tax
+  sta x
+  ldx #62
 clrsprite
   sta $0340, x
+  dex
+  bne clrsprite
+  lda #128
+  sta $0340
+  lda #13
+  sta 2046
+  sta 2047
+  ldx #98
+  stx 53260
+  stx 53261
   inx
-  cpx #62
-  bcc clrsprite
-  lda #1
-  sta $0340, x
+  stx 53262
+  stx 53263
+  lda #192
+  sta 53264
+  sta 53269
+  lda #24
+  sta 16383
+  bit 53278
   cli
   rts
 
 isr
   lda #1
   sta 53273
-  ldx y
-  lda poslo, x
-  sta 251
-  lda poshi, x
-  sta 252
-  ldy x
-  lda (251), y
-  ldx 53278
+  ldx x
+  lda $0400, x
+  ldy 53278
   beq nocol
   ora #128
   bne update_screen
 nocol
   and #127
 update_screen
-  sta (251), y
-  lda 252
-  clc
-  adc #212
-  sta 252
+  sta $0400, x
   lda #1
-  sta (251), y
+  sta $d800, x
   jsr toggle_sprite_pixel
   ldx x
-  ldy y
   inx
   cpx #24
-  bcc updatexy
+  bcc updatex
   ldx #0
-  iny
-  cpy #21
-  bcc updatexy
-  ldy #0
-updatexy
+updatex
   stx x
-  sty y
   jsr toggle_sprite_pixel
   jmp $ea31
 
 toggle_sprite_pixel
-  ldx y
-  lda sposlo, x
-  sta 251
-  lda #3
-  sta 252
   lda x
   tax
   lsr
@@ -89,26 +85,13 @@ toggle_sprite_pixel
   txa
   and #7
   tax
-  lda (251), y
+  lda $0340, y
   eor bits, x
-  sta (251), y
+  sta $0340, y
   rts
 
 x
   .byte 0
-y
-  .byte 0
-
-poslo
-  .byte 0, 40, 80, 120, 160, 200, 240, 24, 64, 104, 144, 184, 224
-  .byte 8, 48, 88, 128, 168, 208, 248, 32
-
-poshi
-  .byte 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 7
-
-sposlo
-  .byte 64, 67, 70, 73, 76, 79, 82, 85, 88, 91, 94, 97, 100, 103
-  .byte 106, 109, 112, 115, 118, 121, 124
 
 bits
   .byte 128, 64, 32, 16, 8, 4, 2, 1
