a7803afd8ce3d3b29f36ba5d4bcb5ab0ac1ff525
Michiel Boland <michiel@boland.org>
2025-06-23 22:13:34 +0200

    Adjust blank
    Make blank work on old kernels.
    Enable sprites at strategic Y-positions.

--- asm/blank.s
+++ asm/blank.s
@@ -1,15 +1,30 @@
   .include "bootstrap.s"
 
 cls = $e544
+row = 12
+scrbase = 1024
+colbase = 55296
 
+  lda #255
+  sta 53269
+  lda #203
+  ldx #15
+  sec
+.l0
+  sta 53248, x
+  sbc #21
+  dex
+  dex
+  bpl .l0
   lda #12
   sta 53280
   sta 53281
-  lda #11
-  sta 646
   jsr cls
   lda #101
-  sta 1504
+  sta scrbase + 40 * row
   lda #103
-  sta 1543
+  sta scrbase + 40 * row + 39
+  lda #11
+  sta colbase + 40 * row
+  sta colbase + 40 * row + 39
   .byte 2
