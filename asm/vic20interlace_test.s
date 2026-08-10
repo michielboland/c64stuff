ae589ed3e596fcf5b0c66108a26027cd3a5c1627
Michiel Boland <michiel@boland.org>
2026-08-10 20:06:43 +0200

    Add vic20interlace_test.s

---
+++ asm/vic20interlace_test.s
@@ -0,0 +1,56 @@
+  .include "vic20bootstrap.s"
+
+cr0 = $9000
+cr1 = $9001
+cr2 = $9002
+cr3 = $9003
+cr4 = $9004
+cr5 = $9005
+cre = $900e
+crf = $900f
+
+  sei
+  lda #<isr
+  sta $0314
+  lda #>isr
+  sta $0315
+  cli
+  rts
+
+isr
+  ldx #$0b
+  lda #60 ; roughly halfway down the screen
+.l0
+  cmp cr4
+  bne .l0
+.l1
+  cmp cr4
+  beq .l1
+  
+  ; LSB of raster counter is now guaranteed to be zero
+  jsr delay ; delay 62 cycles
+  bit cr3
+  bmi .l
+  bit 0
+  nop
+.l:
+  jsr delay
+  bit cr3
+  bmi *+2
+  bmi *+2
+  jsr delay
+  bit cr3
+  bpl *+2
+
+  stx crf
+  ldx #$1b
+  stx crf
+  jmp $eabf
+
+delay:
+  ldy #8
+.l
+  dey
+  bne .l
+  bit 0
+  rts
